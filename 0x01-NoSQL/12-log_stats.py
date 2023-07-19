d1aa9e9d105674141ab34e7b9e6699a8f1e068b8
uthman durosinlohun <udurosinlohun@gmail.com>
2023-07-19 05:14:57 -0700

    12 log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -9,30 +9,15 @@ def getlogs():
     db = client["logs"]
     collection = db.nginx
     logs = collection.count_documents({})
-    get = collection.count_documents({"method": "GET"})
-    post = collection.count_documents({"method": "POST"})
-    put = collection.count_documents({"method": "PUT"})
-    patch = collection.count_documents({"method": "PATCH"})
-    delete = collection.count_documents({"method": "DELETE"})
+    print("{} logs".format(logs))
+    method_list = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in method_list:
+        count = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
     path = collection.count_documents({"path": "/status"})
-    print("""{} logs\nMethods:
-    method GET: {}\t
-    method POST: {}\t
-    method PUT: {}\t
-    method PATCH: {}\t
-    method DELETE: {}\n{} status check""".format(logs, get, post,\
-            put, patch, delete, path))
+    print("{} status check".format(path))
 
 
 if __name__ == "__main__":
     getlogs()
-
-
-
-"""{logs} logs\nMethods:\t
-    method GET: {get}\t
-    method POST: {post}\t
-    method PUT: {put}\t
-    method PATCH: {patch}\t
-    method DELETE: {delete}\n{path} status check
-"""
