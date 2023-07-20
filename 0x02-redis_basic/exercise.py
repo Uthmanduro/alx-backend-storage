dadc6c779e255190352e48a3d3891243e9e3e000
Uthman Durosinlohun <Udurosinlohun@gmail.com>
2023-07-20 02:15:41 +0000

    implement count

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,19 @@
 import uuid
 import redis
 from typing import Union, Callable, Optional
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """a generator function that count how many times the class is called"""
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrapper function that performs the operation"""
+        self._redis.incrby(key, 1)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -12,13 +25,14 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, int, bytes, float]) -> str:
         """returns a string"""
         random_id = str(uuid.uuid4())
         self._redis.mset({random_id: data})
         return random_id
 
-    def get(self, key: str, fn: Optional[Callable]) -> str:
+    def get(self, key: str, fn: Optional[Callable] = None) -> str:
         """converts data back tp desired format"""
         data = self._redis.get(key)
         if fn is not None:
